7fb2672e04c6af4601257a0c345210ad5065ed79
Han Liu <cn.liuhan@gmail.com>
2011-09-12 15:18:38 +0800

    first working version variant commit

---
+++ include/variantmap.h
@@ -0,0 +1,45 @@
+/* 
+ *  Copyright (c) 2011,
+ *  Han Liu (cn.liuhan@gmail.com)
+ *
+ *  This file is changed from crtmpserver (http://www.rtmpd.com/)
+ *  crtmpserver is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  crtmpserver is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with crtmpserver.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef _VARIANTMAP_H
+#define	_VARIANTMAP_H
+
+#include "variant.h"
+
+struct VariantMap {
+	string typeName;
+	map<string, Variant> children;
+	bool isArray;
+
+	VariantMap(VariantMap & variantMap) {
+		typeName = variantMap.typeName;
+		children = variantMap.children;
+		isArray = variantMap.isArray;
+	}
+
+	VariantMap() {
+		isArray = false;
+	}
+};
+
+
+#endif	/* _VARIANTMAP_H */
+
+
---
+++ nbproject/Makefile-Release.mk
@@ -0,0 +1,113 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-MacOSX
+CND_CONF=Release
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/src/platform/osx/osxplatform.o \
+	${OBJECTDIR}/tinyxml/tinyxml.o \
+	${OBJECTDIR}/tinyxml/tinyxmlerror.o \
+	${OBJECTDIR}/src/variant.o \
+	${OBJECTDIR}/tinyxml/tinyxmlparser.o \
+	${OBJECTDIR}/src/platform/baseplatform.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/liblibvariant.dylib
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/liblibvariant.dylib: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -dynamiclib -install_name liblibvariant.dylib -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/liblibvariant.dylib -fPIC ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/src/platform/osx/osxplatform.o: src/platform/osx/osxplatform.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/platform/osx
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/platform/osx/osxplatform.o src/platform/osx/osxplatform.cpp
+
+${OBJECTDIR}/tinyxml/tinyxml.o: tinyxml/tinyxml.cpp 
+	${MKDIR} -p ${OBJECTDIR}/tinyxml
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/tinyxml/tinyxml.o tinyxml/tinyxml.cpp
+
+${OBJECTDIR}/tinyxml/tinyxmlerror.o: tinyxml/tinyxmlerror.cpp 
+	${MKDIR} -p ${OBJECTDIR}/tinyxml
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/tinyxml/tinyxmlerror.o tinyxml/tinyxmlerror.cpp
+
+${OBJECTDIR}/src/variant.o: src/variant.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/variant.o src/variant.cpp
+
+${OBJECTDIR}/tinyxml/tinyxmlparser.o: tinyxml/tinyxmlparser.cpp 
+	${MKDIR} -p ${OBJECTDIR}/tinyxml
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/tinyxml/tinyxmlparser.o tinyxml/tinyxmlparser.cpp
+
+${OBJECTDIR}/src/platform/baseplatform.o: src/platform/baseplatform.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/platform
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/platform/baseplatform.o src/platform/baseplatform.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/liblibvariant.dylib
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
